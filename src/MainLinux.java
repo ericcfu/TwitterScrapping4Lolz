11b6e1ad6d4ce01d9035bdc6b4387208ff844634
Eric Fu <eric.fu0816@gmail.com>
2018-04-15 00:47:16 -0700

    fix things maybe

--- src/MainLinux.java
+++ src/MainLinux.java
@@ -1,18 +1,20 @@
-import javax.swing.JOptionPane;
 /**
  * Main class for launching the TweetAnalyzer from Linux System
  */
+import javax.swing.JOptionPane;
+
 public class MainLinux {
     public static void main(String[] args) {
-    String handle = JOptionPane.showInputDialog("Twitter Handle:");
-    String str_num_tweets = JOptionPane.showInputDialog("How many tweets would you like to analyze?");
-    int num_tweets = Integer.parseInt(str_num_tweets);
+        String handle = JOptionPane.showInputDialog("Twitter Handle:");
+        String str_num_tweets = JOptionPane.showInputDialog("How many tweets would you like to analyze?");
+        int num_tweets = Integer.parseInt(str_num_tweets);
 
-    try {
-        TweetAnalyzer a = new TweetAnalyzer(handle, num_tweets, "LINUX");
-        System.out.print(a.getWordMap());
-    } catch(Exception e) {
-        System.out.println(e);
+        try {
+            TweetAnalyzer a = new TweetAnalyzer(handle, num_tweets, "LINUX");
+
+            System.out.print(a.getWordMap());
+        } catch(Exception e) {
+            System.out.println(e);
+        }
     }
-}
 }
