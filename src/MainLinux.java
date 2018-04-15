122aa735b1444413e2d0430b832b9738e28156da
nathanfong1 <nathanfong@berkeley.edu>
2018-04-15 02:08:04 -0700

    idk what i did

--- src/MainLinux.java
+++ src/MainLinux.java
@@ -1,6 +1,9 @@
 /**
  * Main class for launching the TweetAnalyzer from Linux System
  */
+
+import java.util.TreeMap;
+
 import javax.swing.JOptionPane;
 
 public class MainLinux {
@@ -12,6 +15,17 @@ public class MainLinux {
         try {
             TweetAnalyzer a = new TweetAnalyzer(handle, num_tweets, "LINUX");
             JOptionPane.showMessageDialog(null, handle + "'s longest word is: " + a.getWordMap().firstEntry().getKey());
+
+            TreeMap<Integer, Integer> lm = a.getLengthMap();
+            int totalSize = 0;
+            int numberOfCountedTweets = 0;
+            for (Integer key : lm.keySet()) {
+                totalSize += lm.get(key) * key;
+                numberOfCountedTweets += lm.get(key);
+            }
+            double avgWordLength = ((double) totalSize) / numberOfCountedTweets;
+
+            JOptionPane.showMessageDialog(null, handle + "'s average word length is: " + avgWordLength);
         } catch (Exception e) {
             System.out.println(e);
 
