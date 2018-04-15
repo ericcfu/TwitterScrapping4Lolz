a2effcbc8c00d6ec6ac5e6a3714af91b0bc83f72
Jerry Song <jerrysong@berkeley.edu>
2018-04-14 21:30:23 -0700

    stuff

--- src/TweetAnalyzer.java
+++ src/TweetAnalyzer.java
@@ -1,19 +1,48 @@
+import jdk.nashorn.api.tree.Tree;
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
+import static javafx.scene.input.KeyCode.T;
 
 public class TweetAnalyzer {
+
+    private class Node {
+        private String msg;
+        private int priority;
+
+        private Node(String m, int p) {
+            msg = m;
+            priority = p;
+        }
+
+        private void changePriority(int p) {
+            priority = p;
+        }
+    }
 
     public static void main(String[] args) {
         //Process p = Runtime.getRuntime().exec("python mdfdsjf.py");
 
         File nathan = new File("losebags.txt");
 
+        TreeMap<String, Integer> eric = new TreeMap<>(c);
+
+//        TreeSet<String> emma = new TreeSet<>();
+
+
         try {
             Scanner jerry = new Scanner(nathan);
             while (jerry.hasNext()) {
-                System.out.println(jerry.next());
+                String coke = jerry.next();
+                System.out.println(coke);
+                if (eric.get(coke) == null) {
+                    eric.put(coke, 1);
+                } else {
+                    eric.put(coke, eric.remove(coke) + 1);
+                }
+                System.out.println(eric.toString());
             }
 
         } catch (FileNotFoundException e) {
