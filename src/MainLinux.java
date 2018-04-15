0c453fac61bd93560c793a134f8e3d9d07e76a73
Eric Fu <eric.fu0816@gmail.com>
2018-04-14 23:14:58 -0700

    asdf

--- src/MainLinux.java
+++ src/MainLinux.java
@@ -2,7 +2,6 @@
  * Main class for launching the TweetAnalyzer from Linux System
  */
 public class MainLinux {
-    
     public static void main(String[] args) {
         TweetAnalyzer.main("MAC");
     }
